abfe1b6444ecfc220854ac98bb83aca7d5653e4c
Bharathgoud07 <bharathandhyala@gmail.com>
2025-06-25 15:19:42 +0530

    javascriptday2

---
+++ dom.js
@@ -0,0 +1,9 @@
+
+document.title = "Dom in jscript"; //used to change the title in html
+document.getElementById("header").innerHtml = "hello world!";  //used to manpulate the data
+document.getElementById("para").style.color="pink";
+document.getElementById("para").style.fontsize="30px";
+document.body.style.backgroundColor="grey";
+document.getElementsByClassName("cls")[0].style.color="blue";
+document.getElementById("para").style.fontSize="30px";
+document.body.style.backgroundImage = "url('imagesbg.jpeg')";
